22638534974725d56e03f8584207350fc79a2e4c
Kora Hughes <korashughes@gmail.com>
2025-04-17 18:49:46 -0400

    Added Template Experiment + MOP Analysis

--- painModels.py
+++ painModels.py
@@ -1,47 +1,46 @@
 # Note: all of these are assumptions, in future implementations we can make these unknown
 """ Intended application
 Run optimization trial & store attempted stimulation params in an array.
-Calculate the pain per stimulation event in that trial using pain_function().
-Evaluate model pain via total_pain()
+Calculate the institaneous pain per stimulation event in that trial using instant_pain().
+Feed that into the pain_function() to inject the relative pain.
+Evaluate model pain via pain_recall()
 """
 
 MAX_PAIN = 10
-def pain_function(trials, max_params=[200, 500], weights=[3,2], include_rpd=True):
-    """
-    Input: array of model parameters for each n stimulation events
-            can also weight the degree to which parameters contribute to pain
-    Output: absolute pain score per stim event
-    """
+def instant_pain(params, max_params=[5, 50], weights=[3,2]):
+    """ Input: params = [amplitude, pulse_width], maximum bounds of those params, weight
+        Output: [pain]"""
     # *Note: amplitude in microvolts, pulse-width in microseconds
-    pain = []
-    for i in range(len(trials)):
-        this_pain = instant_pain(trials[i], max_params, weights)
-        if include_rpd:  # tldr exaserbates the change in percieved pain +-[0,1]
-            last_pain = 0 if len(pain) == 0 else pain[-1]
-            this_pain += RPD(last_pain, this_pain)
-            if this_pain > MAX_PAIN:
-                this_pain = MAX_PAIN
-            elif this_pain < 0:
-                this_pain = 0
-        pain.append(this_pain)
-    return pain
-
-def instant_pain(params, max_params, weights):
-    """ params = [amplitude, pulse_width] """
     assert len(params) == len(weights) == len(max_params), "input parameters, weights, and maximum params must be identical in length"
     pain = 0
     for i in range(len(params)):  # for each parameter to our waveform
         pain += weights[i] * (params[i]/max_params[i])
     return pain
 
-def RPD(last_pain, curr_pain, include_lower=True, deg=2):
-    """ principle 2 - relative pain distortion
+def relative_pain(last_pain, curr_pain, include_lower=True, deg=2):
+    """ principle 2 - relative pain distortion - RPD
     calculates the distortion in pain reported """
     if curr_pain > last_pain:
         return (abs(curr_pain - last_pain)/MAX_PAIN)**deg
     elif include_lower:
         return -1 * (abs(curr_pain - last_pain)/MAX_PAIN)**deg
     return 0
+
+def add_relative_pain(painful_trials, weight=2, include_lower=True):
+    """ inject the relative pain into a set of painful trials 
+    Note: weight is the maximum change in pain that can occur due to RPD """
+    pain = []
+    for i, p in enumerate(painful_trials):
+        this_pain = p
+        last_pain = 0 if i == 0 else painful_trials[i-1]
+        this_pain += relative_pain(last_pain, this_pain, include_lower) * weight
+        # add bounds for pain scale
+        if this_pain > MAX_PAIN:
+            this_pain = MAX_PAIN
+        elif this_pain < 0:
+            this_pain = 0
+        pain.append(this_pain)
+    return pain
 
 """ end of study pain recall EOS_recall --> mean, peak, end
 # estimated to be the average reported EOD/peak
@@ -53,17 +52,45 @@ according to these estimates: people report a EOD pain of:
 """
 peak_mod = 46.5 / (41.6+13.9)
 end_mod = 46.5 / (41.6+2.3)
-def MOP(pain_scores):
-    """ memory of pain according to peak-end rule """
-    return peak_mod*max(pain_scores) + end_mod*pain_scores[-1]
+def memory_pain(pain_scores):
+    """ principle 1 - memory of pain - MOP
+    calculates the patient's memory of a set of painful events
+    in accordance with the peak-end rule """
+    return (peak_mod*max(pain_scores) + end_mod*pain_scores[-1]) / 2
+
 mean_mod = 46.5 / 41.6
-def pain_recall(pain_scores, include_mean=True):
+def pain_recall(pain_scores):
     """ leverages memory of pain along with average pain to estimate the
         total pain the patient recalls
         takes a weighted average of the peak, end, and mean estimates
     """
-    mop = MOP(pain_scores)
-    if include_mean:
-        pain_mean = mean_mod*sum(pain_scores)/len(pain_scores)
-        return (mop + pain_mean)/3
-    return mop/2
+    mop = memory_pain(pain_scores)
+    pain_mean = mean_mod*sum(pain_scores)/len(pain_scores)
+    return (2*mop + pain_mean)/3
+
+
+# helper for testing
+import pandas as pd
+def calculate_pains(experiment, max_params=[5,50], param_weights=[5,3], rpd_weight=2):
+    # TODO: make names versatile in case the params change
+    exp_names = ["Amplitude (nA)", "Pulse Width (ms)", "Stimulation Event",
+            "Instant Pain", "Distorted Pain", "Memory of Pain", "Total Recalled Pain"]
+    stim_events = len(experiment)
+    # calculate instantenous pain
+    inst_pain = []
+    for amp, pw in experiment:
+        this_pain = instant_pain([amp, pw], max_params, param_weights)
+        inst_pain.append(this_pain)
+    distorted_pain = add_relative_pain(inst_pain, rpd_weight, True)
+    # calculate memory of/recalled pain
+    mem_pain = memory_pain(distorted_pain)
+    recalled_pain = pain_recall(distorted_pain)
+
+    # construct dataframe
+    exp = pd.DataFrame(experiment, columns=exp_names[:2])
+    exp[exp_names[2]] = [i+1 for i in range(stim_events)]  # add labeling for order
+    exp[exp_names[3]] = inst_pain  # add inst pain
+    exp[exp_names[4]] = distorted_pain  # add pain with RPD distortion
+    exp[exp_names[5]] = [mem_pain for i in range(stim_events)]  # add memory pain
+    exp[exp_names[6]] = [recalled_pain for i in range(stim_events)]  # add recalled pain
+    return exp
